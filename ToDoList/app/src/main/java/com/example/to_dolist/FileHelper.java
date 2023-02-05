26dc14a5455e96d801ec63ee42b8f11339066cfb
kabirgupta <48851690+kabirgupta@users.noreply.github.com>
2023-02-04 21:28:36 -0500

    Add files via upload

---
+++ ToDoList/app/src/main/java/com/example/to_dolist/FileHelper.java
@@ -0,0 +1,43 @@
+package com.example.to_dolist;
+
+import android.content.Context;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class FileHelper {
+
+    public static final String FILENAME="listinfo.dat";
+
+    public static void writeData(ArrayList<String> item, Context context)
+    {
+        try {
+            FileOutputStream fileOutputStream= context.openFileOutput(FILENAME,Context.MODE_PRIVATE);
+            ObjectOutputStream objectOutputStream= new ObjectOutputStream(fileOutputStream);
+            objectOutputStream.writeObject(item);
+            objectOutputStream.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static ArrayList<String> readData(Context context) {
+        ArrayList<String> itemList = null;
+        try {
+            FileInputStream fileInputStream = context.openFileInput(FILENAME);
+            ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+            itemList = (ArrayList<String>) objectInputStream.readObject();
+        } catch (FileNotFoundException e) {
+            itemList=new ArrayList<>();
+            e.printStackTrace();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return itemList;
+    }
+}
